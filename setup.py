e8a5624725080bd0971369d0737e67c91c7904b2
dummyjenil <dummyjenil@gmail.com>
2025-01-25 23:11:13 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='dash2html',
     packages=find_packages(),
-    version='1.0.1',
+    version='1',
     description='Convert Plotly Dash App To Static HTML File',
     author='Jenil sheth',
     author_email="shethjeniljigneshbhai@gmail.com",
